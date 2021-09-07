37eafa552d24f75f5f7b1e03e98831a58a8d9ae6
Anders Norrbom <Anders.Norrbom@kindredgroup.com>
2021-09-07 13:09:02 +0200

    cleanup

--- terraform/prod/static-web-site/main.tf
+++ terraform/prod/static-web-site/main.tf
@@ -8,9 +8,6 @@ module "static-web-site" {
     "Action" = "s3:GetObject"
   }
   policy_allowed_source_ip =[
-    "85.184.100.112/28",
-    "85.184.100.160/27",
-    "185.9.100.0/27",
-    "192.121.200.0/27"
+    "0.0.0.0"
     ]
 }
